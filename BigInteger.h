fbbbbcd756082a62c718e19a6d7c6daf9094b116
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-10-26 19:35:42 +0530

    .h file

---
+++ BigInteger.h
@@ -0,0 +1,32 @@
+int calculate_len(struct BigInteger *head);
+
+int comp(struct BigInteger *head1, struct BigInteger *head2);
+
+void deletefromhead(struct BigInteger **head);
+
+int greater(struct BigInteger*head1,struct BigInteger* head2);
+
+void Head(struct BigInteger **head,int key);
+
+void reverseLL(struct BigInteger **head);
+
+void Tail(struct BigInteger **head,int key);
+
+struct BigInteger *initialize(char a[]);
+
+void deletefromtail(struct BigInteger **head);
+
+void DisplayLL(struct BigInteger *head);
+
+void display(struct BigInteger *head);
+
+struct BigInteger *add(struct BigInteger *head1, struct BigInteger *head2);
+
+struct BigInteger* Subtraction(struct BigInteger *head1,struct BigInteger *head2);
+
+struct BigInteger* sub(struct BigInteger *head1,struct BigInteger *head2);
+
+struct BigInteger* mul(struct BigInteger *head1, struct BigInteger *head2);
+
+struct BigInteger* div1(struct BigInteger *head1 ,struct BigInteger *head2);
+
